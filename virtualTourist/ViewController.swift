55b25d12ca1a2a3aff3d372c3676f4cc0f1e3d10
kurt <kurcey@yahoo.com>
2019-06-22 11:33:49 -0400

    Initial Commit

---
+++ virtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  virtualTourist
+//
+//  Created by user152630 on 6/22/19.
+//  Copyright © 2019 user152630. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
